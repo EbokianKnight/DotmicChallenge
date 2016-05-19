cc6a9da8d2791629ef56de95ea2bc865d7c3b51d
Adam <ebokianknight@gmail.com>
2016-05-19 03:15:03 -0400

    added selector class placement during render

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,57 @@
 import React from 'react';
-import { sortByDate, sortByAuthor, sortByCount } from '../actions/article_actions';
+import { ArticleStore } from '../stores/article_store';
+import { sortByDate, sortByAuthor, sortByCount, sortByDefault }
+  from '../actions/article_actions';
 
-function Header() {
-  return (
-    <div>
-      <header className="header-container">
-        <div className="main-row">
-          <p>Unpublished Articles (66)</p>
-        </div>
-        <div className="sub-row-index">
-          <button className="sub-row header" onClick={sortByAuthor}>Author</button>
-          <button
-            className="sub-row header u-color-alt"
-            onClick={sortByCount}
-          >
-            Words
-          </button>
-          <button
-            className="sub-row header u-color-alt"
-            onClick={sortByDate}
-          >
-            Submitted
-          </button>
-        </div>
-      </header>
-    </div>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sortBy: 'none' };
+    this.setStateFromStore = () => {
+      this.setState({ sortBy: ArticleStore.sortedBy() });
+    };
+  }
+  componentDidMount() {
+    this.storeToken = ArticleStore.addListener(this.setStateFromStore);
+  }
+  componentWillUnmount() {
+    this.storeToken.remove();
+  }
+  render() {
+    let count = 'sub-row header u-color-alt';
+    let author = 'sub-row header';
+    let date = 'sub-row header u-color-alt';
+    if (this.state.sortBy === 'count') {
+      count.append(' selected');
+    } else if (this.state.sortBy === 'author') {
+      author.append(' selected');
+    } else if (this.state.sortBy === 'date') {
+      date.append(' selected');
+    }
+    return (
+      <div>
+        <header className="header-container">
+          <div className="main-row" onClick={sortByDefault}>
+            <p>Unpublished Articles (66)</p>
+          </div>
+          <div className="sub-row-index">
+            <button className={author} onClick={sortByAuthor}>
+              Author
+            </button>
+            <button className={count} onClick={sortByCount}>
+              Words
+            </button>
+            <button className={date} onClick={sortByDate}>
+              Submitted
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
+
+Header.propTypes = { sortBy: React.PropTypes.string };
+Header.defaultProps = { sortBy: ArticleStore.sortedBy() };
 
 export default Header;
