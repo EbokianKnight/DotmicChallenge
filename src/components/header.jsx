b1d3db5b6fd5f2c3c6b1a67371737dbae022907c
Adam <ebokianknight@gmail.com>
2016-05-19 01:22:34 -0400

    Polished CSS

---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { sortByDate, sortByAuthor, sortByCount } from '../actions/article_actions';
+
+function Header() {
+  return (
+    <div>
+      <header className="header-container">
+        <div className="main-row">
+          <p>Unpublished Articles (66)</p>
+        </div>
+        <div className="sub-row-index">
+          <button className="sub-row header" onClick={sortByAuthor}>Author</button>
+          <button
+            className="sub-row header u-color-sortable"
+            onClick={sortByCount}
+          >
+            Words
+          </button>
+          <button
+            className="sub-row header u-color-sortable"
+            onClick={sortByDate}
+          >
+            Submitted
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
