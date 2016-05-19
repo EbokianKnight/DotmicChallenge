4f30b483dd8d75d0fb2a8dcae75b92bb09a326c8
Adam <ebokianknight@gmail.com>
2016-05-19 16:32:29 -0400

    Refactored a few lines of code

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import Header from './header';
 import ArticleStore from '../stores/article_store';
+import { getNextPage } from '../actions/article_actions';
 import RowContainer from './row_container';
 import Footer from './footer';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { articles: ArticleStore.provideArticles() };
-    this.renderArticles = this.renderArticles.bind(this);
-    this.getStateFromStore = () => {
-      this.setState({ articles: ArticleStore.provideArticles() });
+    this.state = {
+      articles: ArticleStore.provideArticles(),
+      complete: ArticleStore.noMoreResults(),
     };
+    this.renderArticles = this.renderArticles.bind(this);
+    this.getStateFromStore = this.getStateFromStore.bind(this);
   }
   componentDidMount() {
     this.token = ArticleStore.addListener(this.getStateFromStore);
+    getNextPage();
   }
   componentWillUnmount() {
     this.token.remove();
   }
+  getStateFromStore() {
+    this.setState({
+      articles: ArticleStore.provideArticles(),
+      complete: ArticleStore.noMoreResults(),
+    });
+  }
   renderArticles() {
-    /*eslint-disable */
-    console.log(this.state)
-    /*eslint-enable */
     return this.state.articles.map(row =>
       <RowContainer key={row.id} article={row} />
     );
@@ -34,13 +40,19 @@ class Main extends React.Component {
         <div className="row-index-wrapper">
           {this.renderArticles()}
         </div>
-        <Footer />
+        <Footer complete={this.state.complete} />
       </div>
     );
   }
 }
 
-Main.PropTypes = { articles: React.PropTypes.array };
-Main.defaultProps = { articles: [] };
+Main.PropTypes = {
+  articles: React.PropTypes.array,
+  complete: React.PropTypes.boolean,
+};
+Main.defaultProps = {
+  articles: [],
+  complete: false,
+};
 
 export default Main;
